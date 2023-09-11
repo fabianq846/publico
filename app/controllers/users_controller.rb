ba59be07af4bfc6dcdd17163e1e057cb644d718a
fabianq846 <fabianq846@gmail.com>
2023-09-11 22:11:08 +0000

    Usuarios editar y eliminar, falta los controladores para realizar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,22 +26,28 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
-
+  
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to @user, notice: 'User was successfully updated.'
+      redirect_to users_path, notice: 'User was successfully updated.'
     else
       render :edit
     end
   end
-
+  
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path, notice: 'User was successfully destroyed.'
+  end
+  
   private
-
+  
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
   end
-  
+    
   
 
   def ensure_esteban
