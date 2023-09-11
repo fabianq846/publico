7ad2400ecde9b22b65586a4bb19e2e6414870230
fabianq846 <fabianq846@gmail.com>
2023-09-11 13:23:26 +0000

    Agrega sedd usuarios y arregla pagina de todos los usuarios

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+# Creando a Esteban si no existe
+esteban = User.find_or_create_by(email: 'esteban@example.com') do |user|
+  user.password = 'password123'
+  user.name = 'Esteban'
+end
+
+# Creando más usuarios
+5.times do |i|
+  User.find_or_create_by(email: "user_seed#{i}@example.com") do |user|
+    user.password = 'password123'
+    user.name = "Usuario Seed #{i}"
+  end
+end
+
+# Creando ofertas laborales
+5.times do |i|
+  JobOffer.create!(title: "Puesto Seed #{i}", description: "Descripción del puesto #{i}")
+end
